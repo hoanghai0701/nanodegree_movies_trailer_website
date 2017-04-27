a6a732e8035b37a968b20b82043f62640a2a600d
hainh <zeta@gotitapp.co>
2017-04-27 16:10:53 +0700

    Add doc string to class Movie

--- media.py
+++ media.py
@@ -1,4 +1,13 @@
 class Movie(object):
+    """
+    This class represents movie object.
+    
+    Attributes:
+        title (str): Title of the movie
+        story_line (str): A short description for the movie
+        poster_image_url (str): An URL of the poster image
+        trailer_youtube_url (str): An URL of the youtube trailer
+    """
     def __init__(self, title, story_line, poster_img_url, trailer_youtube_url):
         self.title = title
         self.story_line = story_line
